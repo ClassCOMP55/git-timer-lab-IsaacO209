0c1e8769cb3d703a9e473f98c3b80d5e5a1ed1a8
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-02-22 18:59:46 -0800

    Inside moveAllBallsOnce() added if statement to check for enemy
    collision and removes them from screen and array list.

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -94,6 +94,12 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 	private void moveAllBallsOnce() {
 		for(GOval ball:balls) {
 			ball.move(SPEED, 0);
+			if (getElementAt(ball.getX()+ball.getWidth()+1,ball.getY()+ball.getHeight()/2) instanceof GRect) {
+				GObject target =getElementAt(ball.getX()+ball.getWidth()+1,ball.getY()+ball.getHeight()/2);
+				remove(target);
+				enemies.remove(target);
+			}
+			
 		}
 	}
 	
